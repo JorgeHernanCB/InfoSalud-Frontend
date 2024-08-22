d3570e2c83b7eab0d76c672752d635ed082fd940
juansm1664 <juansm3007@gmail.com>
2024-08-22 10:53:26 -0500

    setting-sidebar-menu

--- src/app/shared/components/settings-menu/settings-menu.component.ts
+++ src/app/shared/components/settings-menu/settings-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-settings-menu',
+  selector: 'infoSalud-settings-menu',
   templateUrl: './settings-menu.component.html',
   styleUrl: './settings-menu.component.css'
 })
 export class SettingsMenuComponent {
 
+  public settingSidebar: boolean = false;
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,13 +1,61 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'infoSalud-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-
   public sidebarVisible: boolean = false;
 
+  public itemsSidebar: MenuItem[] = [];
+
+  ngOnInit() {
+    this.itemsSidebar = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-home',
+        routerLink: ['/home'],
+        items: [
+          {
+            label: 'pagina1',
+            icon: 'pi pi',
+            routerLink: ['/home/pagina1'],
+          },
+          {
+            label: 'pagina2',
+            icon: 'pi pi',
+          },
+          {
+            label: 'pagina3',
+            icon: 'pi pi',
+          },
+        ],
+      },
+      {
+        label: 'Usuarios',
+        icon: 'pi pi-users',
+        routerLink: ['/users'],
+      },
+      {
+        label: 'Roles',
+        icon: 'pi pi-lock',
+        routerLink: ['/roles'],
+      },
+      {
+        label: 'Permisos',
+      },
+    ];
+  }
+  ngAfterViewInit() {
+    const sidebar = document.getElementById('sidebar');
+    const menuSidebar: any = document.getElementById('menusidebar');
+
+    if (sidebar) {
+      sidebar.onclick = () => {
+        sidebar?.classList.toggle('active');
+      };
+    }
+  }
 }
