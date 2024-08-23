90d20cd3d986dca53fb51758b44de509ec688994
juansm1664 <juansm3007@gmail.com>
2024-08-23 14:11:16 -0500

    createComponentBusinessCard

--- src/app/admin/auth/login/login.module.ts
+++ src/app/admin/auth/login/login.module.ts
@@ -4,10 +4,6 @@ import { FormsModule} from '@angular/forms';
 import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
-=======
-
->>>>>>> 7e32f43724d5decb59302ef8c28411ec88668c16
 
 @NgModule({
   declarations: [
---
+++ src/app/shared/components/business-card/business-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'infoSalud-business-card',
+  templateUrl: './business-card.component.html',
+  styleUrl: './business-card.component.css'
+})
+export class BusinessCardComponent {
+
+}
