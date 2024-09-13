848b564a5b73040e22a07091e0cc7acf64f26239
You7Cris <cristan2110239@gmail.com>
2024-09-13 10:08:49 -0500

    Settings providers component and table-service

--- src/app/catalogueOfActivities/pages/proveedores/proveedores.component.ts
+++ src/app/catalogueOfActivities/pages/proveedores/proveedores.component.ts
@@ -118,10 +118,8 @@ export class ProveedoresComponent implements OnInit {
     ];
     this.typeDocument = [
       { identification: 'Cedula de Ciudadania' },
-      { identification: 'Pasaporte' },
-      { identification: 'Cedula de Extranjeria' },
       { identification: 'NIT' },
-      { identification: 'Tarjeta de Identidad' },
+      { identification: 'DNI' },
     ];
     this.numberDocument = [
       { number: '#', typeDocument: 'Cedula de Ciudadania' },
@@ -140,8 +138,7 @@ export class ProveedoresComponent implements OnInit {
       { status: 'Activo'},
       { status: 'Bloqueado'},
       { status: 'Cancelado'}
-
-    ]
+    ];
   }
 
   onSubmit() {
@@ -152,127 +149,4 @@ export class ProveedoresComponent implements OnInit {
     }
   }
 
-  openNew() {
-    this.proveedor = {};
-    this.submitted = false;
-    this.proveedorDialog = true;
-  }
-
-  viewProveedor(proveedor: proveedores) {
-    this.proveedor = { ...proveedor };
-    this.viewDialog = true;
-  }
-
-  deleteSelectedProveedors() {
-    if (this.SelectedProveedor && this.SelectedProveedor.length > 0) {
-      this.confirmationService.confirm({
-        message: '¿Estás seguro de eliminar los proveedores seleccionados?',
-        header: 'Confirmar',
-        icon: 'pi pi-exclamation-triangle',
-        accept: () => {
-          this.proveedores = this.proveedores.filter(
-            (val) => !this.SelectedProveedor?.includes(val)
-          );
-          this.SelectedProveedor = [];
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Éxito',
-            detail: 'Proveedores eliminados',
-            life: 3000,
-          });
-        },
-      });
-    }
-  }
-
-  editProveedor(proveedor: proveedores) {
-    this.proveedor = { ...proveedor };
-    this.proveedorDialog = true;
-  }
-
-  deleteProveedor(proveedor: proveedores) {
-    this.confirmationService.confirm({
-      message: 'Estas seguro de eliminar ' + proveedor.name + '?',
-      header: 'Confirmar',
-      icon: 'pi pi-exclamation-traingle',
-      accept: () => {
-        this.proveedores = this.proveedores.filter(
-          (val) => val.id !== proveedor.id
-        );
-        this.proveedor = {};
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Succesful',
-          detail: 'Proveedor eliminado',
-          life: 3000,
-        });
-      },
-    });
-  }
-
-  // Cierra el dialogo de edicion
-  hideDialog() {
-    this.proveedorDialog = false;
-    this.submitted = false;
-  }
-
-  //Cierra el dialogo de solo lectura
-  hideViewDialog() {
-    this.viewDialog = false;
-  }
-
-  saveProveedor() {
-    this.submitted = true;
-
-    if (this.proveedorsForm.valid) {
-      if (this.proveedor.id) {
-        // Asegúrate de que id esté definido y sea una cadena
-        const index = this.findIndexById(this.proveedor.id);
-        if (index !== -1) {
-          this.proveedores[index] = this.proveedor;
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Éxito',
-            detail: 'Proveedor actualizado',
-            life: 3000,
-          });
-        } else {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Proveedor no encontrado',
-            life: 3000,
-          });
-        }
-      } else {
-        // Agregar nuevo proveedor
-        this.proveedor.id = this.createId(); // Asignar un nuevo ID
-        this.proveedores.push(this.proveedor);
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Éxito',
-          detail: 'Proveedor agregado',
-          life: 3000,
-        });
-      }
-
-      this.proveedorDialog = false; // Cerrar diálogo
-      this.proveedor = {}; // Limpiar objeto de proveedor
-    }
-  }
-
-  findIndexById(id: string | number): number {
-    return this.proveedores.findIndex((proveedor) => proveedor.id === id);
-  }
-
-  createId(): number {
-    return Math.floor(Math.random() * 100000);
-  }
-
-  applyGlobalFilter(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    if (this.dt1) {
-      this.dt1.filterGlobal(inputElement.value, 'contains');
-    }
-  }
 }
