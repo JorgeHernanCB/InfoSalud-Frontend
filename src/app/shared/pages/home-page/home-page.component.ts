e8a65df4eee9a2f8008644b170cd922b20b3a1ab
juansm1664 <juansm3007@gmail.com>
2024-09-17 08:40:11 -0500

    create-ProgresSpinner-HomePage

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -3,9 +3,11 @@ import { AuthService } from '../../../service/authService/auth-service.service';
 @Component({
   selector: 'infoSalud-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
+  //Variable para el spinner
+  loading: boolean = true;
 
   constructor(private authService: AuthService) {}
 
@@ -13,4 +15,9 @@ export class HomePageComponent {
     this.authService.logout();
   }
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
+  }
 }
